d9702c4a269fd3cc57c1f17e277e50abed53aad9
Estarlin R <dev@estarlincito.com>
2025-06-19 01:59:48 -0400

    mdxlayer@1.0.7

--- packages/next-mdxlayer/src/runtime.ts
+++ packages/next-mdxlayer/src/runtime.ts
@@ -1,34 +1,51 @@
-/* eslint-disable safeguard/no-raw-error */
-/* eslint-disable no-console */
-import { spawn, spawnSync } from 'child_process';
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+/* eslint-disable no-param-reassign */
 
-export const runMdxlayer = (mode: 'dev' | 'build') => {
-  const command = process.platform === 'win32' ? 'npx.cmd' : 'npx';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import path from 'node:path';
 
-  if (mode === 'dev') {
-    const proc = spawn(command, ['mdxlayer', 'dev'], {
-      shell: false,
-      stdio: 'inherit',
-    });
+// @ts-ignore
+import { builder } from 'mdxlayer/builder';
+// @ts-ignore
+import { watcher } from 'mdxlayer/watcher';
 
-    proc.on('error', (err) => {
-      console.error(`[mdxlayer dev] error:`, err);
-    });
+let devProcess = null;
+let buildExecuted = false;
+const isDev = process.argv.includes('dev');
+const isBuild = process.argv.includes('build');
 
-    console.log('> mdxlayer dev started');
-    return proc;
+/**
+ * Next.js plugin to integrate the MDXLayer build system.
+ *
+ * This plugin automatically runs `mdxlayer dev` or `mdxlayer build`
+ * based on the current process arguments. It also injects the `mdxlayer/generated`
+ * alias into the Webpack configuration to enable project-wide access
+ * to the generated files.
+ *
+ * @param {import('next').NextConfig} [nextConfig={}] - Optional Next.js configuration object.
+ * @returns {import('next').NextConfig} - The enhanced Next.js configuration with MDXLayer support.
+ */
+export const withMdxlayer = (nextConfig = {}) => {
+  if (isDev) {
+    devProcess ??= watcher();
   }
 
-  if (mode === 'build') {
-    const result = spawnSync(command, ['mdxlayer', 'build'], {
-      stdio: 'inherit',
-    });
-
-    if (result.status !== 0) {
-      throw new Error('❌ mdxlayer build failed');
+  if (isBuild) {
+    if (!buildExecuted) {
+      buildExecuted = true;
+      builder();
     }
-
-    console.log('> mdxlayer build completed');
   }
-  return null;
+
+  return {
+    ...nextConfig,
+    webpack(config: any) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        'mdxlayer/generated': path.join(process.cwd(), '.mdxlayer/generated'),
+      };
+
+      return config;
+    },
+  };
 };
